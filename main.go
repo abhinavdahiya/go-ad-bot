78bd37038b3df101681ae651dabafbe568a6b692
Abhinav Dahiya <abhinavdtu2012@gmail.com>
2016-04-15 16:28:12 +0530

    fixes

--- main.go
+++ main.go
@@ -11,9 +11,6 @@ import (
 
 func main() {
 	bot := mbotapi.NewBotAPI("CAAOmDyEhPfwBAJ9JJpRLvZB3i2dLdJXIZBZCEYYTNJkpUnzHs6KYKN5I1vySuwTmF3uxRrwCHDLQBJJiMHZAXgHiZAV87ugaLR3iYnW1k5OD9KPmvaMAP0rEZCSfUHz0lolyoUelwNJQJdYZAfLElMfhvlGn1ZCPrTXcNNepItZC1509LIGdMK9ZAwZANI9Six9Sp1EB6Ozu0CRtgZDZD", "my_voice_is_my_password")
-	if err != nil {
-		log.Panic(err)
-	}
 
 	callbacks, mux := bot.SetWebhook("/webhook")
 	n := negroni.Classic()
